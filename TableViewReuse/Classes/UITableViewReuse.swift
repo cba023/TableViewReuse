08600e93a613c9782c7d33aea884320251f61f12
cba023 <cba023@hotmail.com>
2024-09-23 18:10:28 +0800

    Update UITableViewReuse.swift

--- TableViewReuse/Classes/UITableViewReuse.swift
+++ TableViewReuse/Classes/UITableViewReuse.swift
@@ -9,20 +9,25 @@ import UIKit
 
 public extension UITableView {
     
+    private func className<Object: AnyObject>(_ cls: Object.Type) -> String {
+        let className = NSStringFromClass(cls)
+        return className
+    }
+    
+    private func generateClass(name: String) -> AnyObject {
+        let cls: AnyClass = NSClassFromString(name)!
+        return cls
+    }
+    
     /// Reuse cell with nibClass and bundle. (Swift)
     /// - Parameters:
     ///   - nibClass: Nib class
     ///   - bundle: Bundle
     /// - Returns: A cell instance
     func dequeueReusableCell<T: UITableViewCell>(nibClass: T.Type, bundle: Bundle = .main) -> T {
-
-        let className = "\(String(describing: nibClass))"
-        var cell = self.dequeueReusableCell(withIdentifier: className)
-        if cell == nil {
-            cell = (bundle.loadNibNamed(className, owner: nil, options: nil)?.first as! T)
-        }
+        let className = String(className(nibClass).split(separator: ".").last ?? "")
+        let cell = dequeueReusableCell(withIdentifier: className) ?? bundle.loadNibNamed(className, owner: nil, options: nil)?.first
         return cell as! T
-        
     }
     
     /// Reuse Cell with any class with bundle. (Swift)
@@ -31,17 +36,14 @@ public extension UITableView {
     ///   - bundle: Bundle
     /// - Returns: A cell instance
     func dequeueReusableCell<T: UITableViewCell>(anyClass: T.Type, bundle: Bundle = .main) -> T {
-        
-        let className = "\(String(describing: anyClass))"
-        var cell = self.dequeueReusableCell(withIdentifier: className)
+        let className = className(anyClass)
+        var cell = dequeueReusableCell(withIdentifier: className)
         if cell == nil {
-            let namespace = bundle.infoDictionary!["CFBundleExecutable"] as! String
-            let cls:AnyObject = NSClassFromString(className == NSStringFromClass(UITableViewCell.self) ? className : namespace + "." + className)!
+            let cls = generateClass(name: className)
             let initClass = cls as! T.Type
             cell = initClass.init(style: .default, reuseIdentifier: className)
         }
         return cell as! T
-        
     }
     
     /// Reuse header or footer with nib and bundle. (Swift)
@@ -50,15 +52,9 @@ public extension UITableView {
     ///   - bundle: Bundle
     /// - Returns: A header or footer instance
     func dequeueReusableHeaderFooterView<T: UIView>(nibClass: T.Type, bundle: Bundle = .main) -> T {
-        
-        let className = "\(String(describing: nibClass))"
-        var headerFooter:UIView? = (self.dequeueReusableHeaderFooterView(withIdentifier: className))
-        // 新创建
-        if headerFooter == nil {
-            headerFooter = ((bundle.loadNibNamed(className, owner: nil, options: nil)?.first) as! UIView)
-        }
+        let className = String(className(nibClass).split(separator: ".").last ?? "")
+        let headerFooter = dequeueReusableHeaderFooterView(withIdentifier: className) ?? ((bundle.loadNibNamed(className, owner: nil, options: nil)?.first) as! UIView)
         return headerFooter as! T
-        
     }
     
     /// Reuse header or footer with bundle. (Swift)
@@ -67,19 +63,13 @@ public extension UITableView {
     ///   - bundle: Bundle
     /// - Returns: A header or footer instance
     func dequeueReusableHeaderFooterView<T: UITableViewHeaderFooterView>(anyClass: T.Type, bundle: Bundle = .main) -> T {
-        
-        let className = "\(String(describing: anyClass))"
-        var headerFooter:UIView? = self.dequeueReusableHeaderFooterView(withIdentifier: className)
+        let className = className(anyClass)
+        var headerFooter:UIView? = dequeueReusableHeaderFooterView(withIdentifier: className)
         if headerFooter == nil {
-            let namespace = bundle.infoDictionary!["CFBundleExecutable"] as! String
-            let cls:AnyObject = NSClassFromString(className == NSStringFromClass(UITableViewHeaderFooterView.self) ? className : namespace + "." + className)!
+            let cls = generateClass(name: className)
             let initClass = cls as! T.Type
             headerFooter = initClass.init(reuseIdentifier: className)
         }
         return headerFooter as! T
-        
     }
 }
-
-
-
